5759c73baba6aa0f4903af74c245fb2b5b735b50
Sarah Elzinga <sevans924@gmail.com>
2019-03-23 03:15:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,8 @@ def summon_captain_planet(planeteer_calls)
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
+  array.any? {|word| word.length > 4}# code an argument here
   # Your code here
 end
 
