04e4419772c30ed44ddfdb5c3e84435649fc59f5
Sarah Elzinga <sevans924@gmail.com>
2019-03-23 03:23:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,5 @@ def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  cheese.find {|is_cheese| cheese.include?(is_cheese)}
+  cheese.include?(cheese_types) 
 end
