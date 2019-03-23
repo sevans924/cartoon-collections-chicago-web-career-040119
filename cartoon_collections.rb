e1ef6f12c851def0704fdc0eb3549ae332f93479
Sarah Elzinga <sevans924@gmail.com>
2019-03-23 03:36:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,7 @@ def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
      if cheese.include?("cheddar")
+       puts true
      else
        nil
 
