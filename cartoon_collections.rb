def199a55df99d90ab60e176e6698d3488b2a7e2
Sarah Elzinga <sevans924@gmail.com>
2019-03-23 03:34:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
      cheese.include?("cheddar")
-   
+
 
 end
